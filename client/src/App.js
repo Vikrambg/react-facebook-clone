c55b640aa388ba26cf7aa5437deef5cfc8ff6ab1
vikram kumar <vikrambhagat0811@gmail.com>
2021-08-08 18:59:54 +0530

    register page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import Register from './pages/Register'
 
 function App() {
   return (
     <>
    {/*  <Home /> */}
    {/* <Profile /> */}
-   <Login />
+   {/* <Login /> */}
+   <Register />
     </>
   );
 }
--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -4,7 +4,22 @@ const Register = () =>{
     return (
         <div className="register">
             <div className="registerWrapper">
-
+                <div className="registerLeft">
+                    <h3 className="loginLogo"> LamaSocial </h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on LamaSocial
+                    </span>
+                </div>
+                <div className="registerRight">
+                    <div className="registerBox">
+                        <input type="text" className="loginInput" placeholder="Full Name" />
+                        <input type="text" className="loginInput" placeholder="Email" />
+                        <input type="text" className="loginInput" placeholder="Password" />
+                        <button className="registerButton">Create Account</button>
+                        <span className="loginForgot">Forgot Password</span>
+                        <button className="registerLoginButton">Login</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
